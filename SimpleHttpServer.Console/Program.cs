36e1190c33870149a6ae2dcc578350b5dce003a9
Jordan Raine <raine.jordan@gmail.com>
2019-04-19 20:49:30 +1000

    Add reference from Console project to Core project, start the server from the Console project

--- SimpleHttpServer.Console/Program.cs
+++ SimpleHttpServer.Console/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace SimpleHttpServer.Console
+﻿using SimpleHttpServer.Core;
+
+namespace SimpleHttpServer.Console
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            Server.Start();
+            System.Console.ReadLine();
         }
     }
 }
